33fe385d3dfa7fe7cb24068c49bd1a8e03828356
GolaJakub <kuba300901@gmail.com>
2023-10-29 10:34:36 +0100

    test commit

---
+++ LAB1/src/main/java/pl/pjatk/tau_pjatk/TauPjatkApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.tau_pjatk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauPjatkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TauPjatkApplication.class, args);
+    }
+
+}
